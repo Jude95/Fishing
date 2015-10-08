24587b5d3189725be2965bcafacc519dd4c4839c
Jude95 <973829691@qq.com>
2015-10-08 20:29:36 +0800

    修改网络请求部分

--- app/src/main/java/com/jude/fishing/config/API.java
+++ app/src/main/java/com/jude/fishing/config/API.java
@@ -5,7 +5,7 @@ package com.jude.fishing.config;
  */
 public class API {
     public static class URL{
-        public static final String BASEURL = "http://123.56.225.103/api/index.php?s=/api/";
+        public static final String BASE_URL = "http:small-fish.daoapp.io/index.php";
         public static final String UpdateLog = "https://raw.githubusercontent.com/Jude95/Fishing/master/update.txt";
 
 
