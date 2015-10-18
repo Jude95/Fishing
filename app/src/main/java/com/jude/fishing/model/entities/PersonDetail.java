e35107e95283a71d7fa94a59d25caec355686bb4
mike <mikekee@qq.com>
2015-10-18 10:30:26 +0800

    他的热闹

--- app/src/main/java/com/jude/fishing/model/entities/PersonDetail.java
+++ app/src/main/java/com/jude/fishing/model/entities/PersonDetail.java
@@ -14,6 +14,7 @@ public class PersonDetail extends PersonBrief implements Serializable {
     @SerializedName("bg")
     private String background;
     private String address;
+    @SerializedName("seed")
     private List<Seed> seeds;
     private int blogCount;
     private String cared;
